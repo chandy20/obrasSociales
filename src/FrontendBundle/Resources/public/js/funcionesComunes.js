b4c5bc83d4eee9bc4ebcd105a43105bd4a725704
Alexander <alexander.correa@ito-software.com>
2018-03-06 14:26:22 -0500

    Ajuste

--- src/FrontendBundle/Resources/public/js/funcionesComunes.js
+++ src/FrontendBundle/Resources/public/js/funcionesComunes.js
@@ -13,14 +13,6 @@ function mostrarFormulario() {
         }
     }
 }
-function soloNumeros(e) {
-    var key = e.charCode || e.keyCode || 0;
-    // allow backspace, tab, delete, arrows, numbers and keypad numbers ONLY
-    return (
-            key == 8 ||
-            key == 9 ||
-            key == 46 ||
-            (key >= 37 && key <= 40) ||
-            (key >= 48 && key <= 57) ||
-            (key >= 96 && key <= 105));
+function soloNumeros(input) {
+    $(input).val($(input).val().replace(/\D/g, ''));
 }
