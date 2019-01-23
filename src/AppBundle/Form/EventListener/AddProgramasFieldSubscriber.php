804caf010882f7c4d70aac0e3d9d0fa544acf102
odeg36@hotmail.com <odeg36@hotmail.com>
2019-01-22 23:19:07 -0500

    arreglo solicitudes

--- src/AppBundle/Form/EventListener/AddProgramasFieldSubscriber.php
+++ src/AppBundle/Form/EventListener/AddProgramasFieldSubscriber.php
@@ -8,26 +8,22 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
-class AddProgramasFieldSubscriber implements EventSubscriberInterface
-{
+class AddProgramasFieldSubscriber implements EventSubscriberInterface {
 
     public $presupuesto = false;
 
-    public function __construct($presupuesto = false)
-    {
+    public function __construct($presupuesto = false) {
         $this->presupuesto = $presupuesto;
     }
 
-    public static function getSubscribedEvents()
-    {
+    public static function getSubscribedEvents() {
         return array(
             FormEvents::PRE_SET_DATA => 'preSetData',
             FormEvents::PRE_SUBMIT => 'preSubmit'
         );
     }
 
-    private function addProgramasForm($form, $padre, $multiple, $valores)
-    {
+    private function addProgramasForm($form, $padre, $multiple, $valores) {
 
         $formOptions = array(
             "class" => "AppBundle:Programas",
@@ -39,16 +35,16 @@ class AddProgramasFieldSubscriber implements EventSubscriberInterface
             'query_builder' => function (EntityRepository $repository) use ($padre) {
                 $padre = $padre ?: 0;
                 $qb = $repository->createQueryBuilder('p')
-                    ->join('p.programa', 'pp')
-                    ->where('pp.id IN (:padre)')
-                    ->orderBy("pp.programanombre", 'DESC')
-                    ->setParameter('padre', $padre);
+                        ->join('p.programa', 'pp')
+                        ->where('pp.id IN (:padre)')
+                        ->orderBy("pp.programanombre", 'DESC')
+                        ->setParameter('padre', $padre);
                 return $qb;
             },
         );
         if ($valores) {
             $programas = [];
-            foreach ($valores as $valor){
+            foreach ($valores as $valor) {
                 $programas[] = $valor->getPrograma();
             }
             $formOptions['data'] = $programas;
@@ -60,8 +56,7 @@ class AddProgramasFieldSubscriber implements EventSubscriberInterface
         }
     }
 
-    public function preSetData(FormEvent $event)
-    {
+    public function preSetData(FormEvent $event) {
         $data = $event->getData();
         $form = $event->getForm();
 
@@ -82,15 +77,20 @@ class AddProgramasFieldSubscriber implements EventSubscriberInterface
         $this->addProgramasForm($form, $padres, property_exists($data, 'programas'), $valores);
     }
 
-    public function preSubmit(FormEvent $event)
-    {
+    public function preSubmit(FormEvent $event) {
         $data = $event->getData();
         $form = $event->getForm();
         if (null === $data) {
             return;
         }
         $padre = array_key_exists('programaPadre', $data) ? $data['programaPadre'] : null;
-        $this->addProgramasForm($form, $padre, array_key_exists('programas', $data), null);
+        $multiple = false;
+        try {
+            $multiple = $form->get('programas') ? true : false;
+        } catch (\Exception $e) {
+            $multiple = false;
+        }
+        $this->addProgramasForm($form, $padre,$multiple, null);
     }
 
 }
